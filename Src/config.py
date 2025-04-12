b187000433e78705ada78a2e54878ac6f1206ca5
Cesar Carrera <c.carrerag96@gmail.com>
2025-04-12 15:41:59 -0500

    Subiendo archivos

---
+++ Src/config.py
@@ -0,0 +1,8 @@
+user = 'CESARCARRERA96'  # Usuario de Snowflake
+password = 'Cesarc1723111017'  # Contraseña de Snowflake
+account = 'NJSGIPU-XM61294'  # Cuenta de Snowflake
+warehouse = 'COMPUTE_WH'  # Warehouse de Snowflake
+database = 'PROYECTO'  # Base de datos de Snowflake
+schema = 'RAW'  # Esquema de Snowflake
+
+
